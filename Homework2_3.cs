a60ba6a8d1b61295af369d67e7b02446739daf09
sangarma <39394325+sangarma@users.noreply.github.com>
2024-01-29 21:48:52 +0545

    Code file for third problem of Homework 2

---
+++ Homework2_3.cs
@@ -0,0 +1,20 @@
+﻿namespace HomeWork2_3;
+
+class Program
+{
+    static void Main()
+    {
+        
+        Console.WriteLine("Please input a year: ");
+        int year = Convert.ToInt16(Console.ReadLine());//take input and change to integer
+
+        if ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0))//condition for leap year
+        {
+            Console.WriteLine($"{year} is a Leap Year.");
+        }
+        else
+        {
+            Console.WriteLine($"{year} is not a Leap Year.");
+        }
+    }
+}
