da2320207d9c5258aacaff4154e0a9afa23179e1
sangarma <39394325+sangarma@users.noreply.github.com>
2024-01-29 21:06:21 +0545

    Code file for homework 1

---
+++ Homework1.cs
@@ -0,0 +1,16 @@
+﻿namespace HomeWork1;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Declare and define variables.
+        float X = 2.5f;
+        float Y = 3.3f;
+        float Z;
+        Z = 4*X*X + 3*Y;
+        // Print the results on terminal.
+        Console.WriteLine($"X = {X}, Y = {Y}");
+        Console.WriteLine($"The value of Z is: {Z}");
+    }
+}
