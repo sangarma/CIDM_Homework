413fa83a5c61878bc19cbdb75845d8514f4ae472
sangarma <39394325+sangarma@users.noreply.github.com>
2024-01-29 21:47:06 +0545

    Code file for first problem of Homework 2

---
+++ Homework2_1.cs
@@ -0,0 +1,41 @@
+﻿namespace HomeWork2_1;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Please input a letter grade:");
+        string gradeLetter = Console.ReadLine();// take input from user
+        char letterGrade = Convert.ToChar(gradeLetter); //change string to character variable
+
+        int gpa;// define gpa variable
+
+        if (letterGrade == 'A')
+        {
+            gpa = 4;
+        }
+        else if (letterGrade == 'B')
+        {
+            gpa = 3;
+        }
+        else if (letterGrade == 'C')
+        {
+            gpa = 2;
+        }
+        else if (letterGrade == 'D')
+        {
+            gpa = 1;
+        }
+        else if (letterGrade == 'F')
+        {
+            gpa = 0;
+        }
+        else
+        {
+            Console.WriteLine("Wrong Letter Grade!");// print the error
+            return; // Exit the program if the letter grade is invalid
+        }
+
+        Console.WriteLine($"GPA Point: {gpa}");//print the gpa
+    }
+}
