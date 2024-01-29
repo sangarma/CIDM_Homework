4c879aeb19f22a18133039ed3a2e7a0afb94c031
sangarma <39394325+sangarma@users.noreply.github.com>
2024-01-29 21:47:57 +0545

    Code file for second problem of Homework 2

---
+++ Homework2_2.cs
@@ -0,0 +1,35 @@
+﻿namespace HomeWork2_2;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+       int num1, num2, num3;//initialize the variables
+       Console.WriteLine("Please input the first num:");
+       num1 = Convert.ToInt16(Console.ReadLine());//take input and convert to integer
+
+       Console.WriteLine("Please input the second num:");
+       num2 = Convert.ToInt16(Console.ReadLine());
+
+       Console.WriteLine("Please input the third num:");
+       num3 = Convert.ToInt16(Console.ReadLine());
+
+       if(num1<num2){
+        if(num1<num3){
+            Console.WriteLine($"The smallest value is: {num1}");
+        }
+        else{//num3 <= num1
+            Console.WriteLine($"The smallest value is: {num3}");
+        }
+       }
+       else{//num2 <= num1
+        if(num3<num2){
+            Console.WriteLine($"The smallest value is: {num3}"); 
+        }
+        else{//num2 <= num3
+            Console.WriteLine($"The smallest value is: {num2}"); 
+        }
+       }
+       
+    }
+}
