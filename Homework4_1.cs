408967123696c6cc704759e2ddb70f8bf337d3ad
sangarma <39394325+sangarma@users.noreply.github.com>
2024-02-10 23:16:27 +0545

    Files for Homework 4

---
+++ Homework4_1.cs
@@ -0,0 +1,24 @@
+﻿namespace HomeWork4_1;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Enter the first number (a):");
+        int a = Convert.ToInt32(Console.ReadLine());// takes first integer from user
+
+        Console.WriteLine("Enter the second number (b):");
+        int b = Convert.ToInt32(Console.ReadLine()); //takes second integer from user
+        Console.WriteLine($"a:{a}; b:{b}");
+        
+        int largestNumber = Islargest(a, b); //calls the function Islargest
+        Console.WriteLine("The largest number is: " + largestNumber); //prints out the result
+    }
+
+
+
+    public static int Islargest(int num1, int num2)
+    {
+        return num1 > num2 ? num1 : num2;//ternary operator to compare number
+    }
+}
---
+++ Homework4_2.cs
@@ -0,0 +1,56 @@
+﻿namespace HomeWork4_2;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Enter a number: ");
+        int N = Convert.ToInt32(Console.ReadLine());// takes integer from user
+
+        Console.WriteLine("Enter the shape('right' or 'left'): ");
+        string shape = Console.ReadLine(); //takes shape from user
+
+        Console.WriteLine( $"N is: {N}; shape is: {shape}"); // print number and the shape
+
+        Pattern(N, shape); //call method
+
+
+
+    }
+
+    public static void Pattern(int N, string shape) //method with no return value
+        {
+            
+            if (shape == "left")
+            {
+                for (int i = 1; i <= N; i++)
+                {
+                    for (int j = 1; j <= i; j++)
+                    {
+                        Console.Write("*");
+                    }
+                    Console.WriteLine();
+                }
+            }
+            else if (shape == "right")
+            {
+                for (int i = 1; i <= N; i++)
+                {
+                    for (int j = N; j >= i; j--)
+                    {
+                        Console.Write(" ");
+                    }
+                    for (int k = 1; k <= i; k++)
+                    {
+                        Console.Write("*");
+                    }
+                    Console.WriteLine();
+                }
+            }
+            else
+            {
+                Console.WriteLine("Invalid shape. Please enter 'left' or 'right'.");// Show error if unsupported shape entered
+            }
+        }
+
+}
